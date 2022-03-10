d3eea41028edb40c083bf9148d7fd120f2545cb0
faizaharb <fandshassan@gmail.com>
2022-03-10 22:47:51 +0200

    Updates

---
+++ precache-manifest.cdaca8424f4da0733375da11764b2dce.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "781886e0a33bad5649ef",
+    "url": "/start-react/static/css/main.ddf1714d.chunk.css"
+  },
+  {
+    "revision": "781886e0a33bad5649ef",
+    "url": "/start-react/static/js/main.781886e0.chunk.js"
+  },
+  {
+    "revision": "efb83b801b56bf7f6525",
+    "url": "/start-react/static/css/1.38c241be.chunk.css"
+  },
+  {
+    "revision": "efb83b801b56bf7f6525",
+    "url": "/start-react/static/js/1.efb83b80.chunk.js"
+  },
+  {
+    "revision": "09269160fed92530dd8d",
+    "url": "/start-react/static/js/runtime~main.09269160.js"
+  },
+  {
+    "revision": "81899e18586b10083308cdf1270c656f",
+    "url": "/start-react/static/media/avataaars.81899e18.svg"
+  },
+  {
+    "revision": "4b3030239ea61aad26dfe7f60521cab2",
+    "url": "/start-react/static/media/404.4b303023.PNG"
+  },
+  {
+    "revision": "56ec4ee72a91e8f3d954db6cccbf8ab2",
+    "url": "/start-react/static/media/cabin.56ec4ee7.png"
+  },
+  {
+    "revision": "a29c0b43958bf3638364e15ff646fffa",
+    "url": "/start-react/static/media/cake.a29c0b43.png"
+  },
+  {
+    "revision": "e27c88e555910ddce50b6f1e275023a4",
+    "url": "/start-react/static/media/circus.e27c88e5.png"
+  },
+  {
+    "revision": "107d31e81b23d07ad38c7cc4d1f7ea27",
+    "url": "/start-react/static/media/game.107d31e8.png"
+  },
+  {
+    "revision": "7dfdcf559d331a0cdc05cc1d2b8c8233",
+    "url": "/start-react/static/media/safe.7dfdcf55.png"
+  },
+  {
+    "revision": "8fad4ff92040b17345896e0e777a79ad",
+    "url": "/start-react/static/media/submarine.8fad4ff9.png"
+  },
+  {
+    "revision": "f817938f131b0cabee81e59a96f9c2a6",
+    "url": "/start-react/static/media/fa-regular-400.f817938f.woff2"
+  },
+  {
+    "revision": "35307505c129ab1f9173f7917b50756f",
+    "url": "/start-react/static/media/fa-regular-400.35307505.ttf"
+  },
+  {
+    "revision": "a9afdb72826cde196ddf29eb8f9d0f8f",
+    "url": "/start-react/static/media/fa-brands-400.a9afdb72.woff2"
+  },
+  {
+    "revision": "297973a488f688271dd223d542ba2697",
+    "url": "/start-react/static/media/fa-solid-900.297973a4.woff2"
+  },
+  {
+    "revision": "72d85419b5a3c5867b1903ef565f5a8d",
+    "url": "/start-react/static/media/fa-v4compatibility.72d85419.woff2"
+  },
+  {
+    "revision": "4bde6a62454b669bb0ce0448491a71d7",
+    "url": "/start-react/static/media/fa-v4compatibility.4bde6a62.ttf"
+  },
+  {
+    "revision": "ffc89d3ea2e2c3ba295689305f91ca78",
+    "url": "/start-react/static/media/fa-solid-900.ffc89d3e.ttf"
+  },
+  {
+    "revision": "0fbfc862056bad982c93ba51460e31d9",
+    "url": "/start-react/static/media/fa-brands-400.0fbfc862.ttf"
+  },
+  {
+    "revision": "fc14af0a42468520deeb2fe5ebc655d9",
+    "url": "/start-react/index.html"
+  }
+];
